fc8b01410ee79f766911839a91fb7e7d303976f7
guru-das-pr <gurudaspr.tltechnologies@gmail.com>
2024-11-11 14:52:15 +0530

    created all auth routes and controllers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { register } from "../controllers/auth.controller.js";
+import { changePassword, forgotPassword, login, register, resetPassword, verifyOtp } from "../controllers/auth.controller.js";
+import verifyjwtToken from "../middlewares/verifyJwtToken.js";
 
 
 
@@ -8,10 +9,12 @@ const router = express.Router();
 
 
 //auth routes
-router.post("/register",register )
-router.post("/login", )
-router.post("/change-password", )
-router.post("/reset-password", )
+router.post("/register", register)
+router.post("/login",login)
+router.post("/change-password",verifyjwtToken,changePassword) // needs jwt token
+router.post("/forgot-password",forgotPassword)
+router.post('/verify-otp',verifyOtp)
+router.post('/reset-password',resetPassword)
 
 
 export default router;
