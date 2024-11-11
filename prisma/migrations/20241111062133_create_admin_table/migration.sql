d5de6025f5117d19732938f880c3ed4a086eaf5d
Thasleem-tltechnologies <thasleem.tltechnologies@gmail.com>
2024-11-11 11:52:19 +0530

    added admin table

---
+++ prisma/migrations/20241111062133_create_admin_table/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- DropTable
+DROP TABLE [dbo].[User];
+
+-- CreateTable
+CREATE TABLE [dbo].[Admin] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [name] NVARCHAR(1000) NOT NULL,
+    [email] NVARCHAR(1000) NOT NULL,
+    [password] NVARCHAR(1000) NOT NULL,
+    CONSTRAINT [Admin_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [Admin_email_key] UNIQUE NONCLUSTERED ([email])
+);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
