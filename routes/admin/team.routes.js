7d29f0cbc11e0f4cb6d0765d6920986557e8fd55
guru-das-pr <gurudaspr.tltechnologies@gmail.com>
2024-11-13 17:55:33 +0530

    added update team and delete team

--- routes/admin/team.routes.js
+++ routes/admin/team.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addTeam, getActiveTeam, getAllTeam} from "../../controllers/team.controller.js";
+import { addTeam, deleteTeamMember, getAllTeam, updateTeam} from "../../controllers/team.controller.js";
 import upload from "../../middlewares/upload.middleware.js";
 import verifyJwtToken from "../../middlewares/verifyJwtToken.js";
 
@@ -8,5 +8,7 @@ const router = express.Router();
 
 router.post('/add-team',verifyJwtToken,upload.single('image'),addTeam);
 router.get('/all-team',verifyJwtToken,getAllTeam)
+router.put('/update-team/:id',upload.single('image'),updateTeam)
+router.delete('/delete-team/:id',deleteTeamMember)
 
 export default router;
