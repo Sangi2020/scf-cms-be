2c968149f0e5de99e2549733d0dad2735a666465
guru-das-pr <gurudaspr.tltechnologies@gmail.com>
2024-11-27 10:26:38 +0530

    updated notification controllers and routes

--- routes/admin/notification.routes.js
+++ routes/admin/notification.routes.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import { getNotifications } from '../../controllers/notification.controller.js';
+import { deleteAllNotifications, deleteNotification, getNotifications, markAllAsRead, markAsRead, triggerNotification } from '../../controllers/notification.controller.js';
 
 
 const router = express.Router();
 
 router.get('/get-all-notifications', getNotifications);
+router.put('/mark-as-read/:id', markAsRead);
+router.put('/mark-all-as-read',markAllAsRead)
+router.delete('/delete/:id',deleteNotification)
+router.delete('/clear-all-notifications', deleteAllNotifications);
+
+
+
+
+//testing api
+router.post('/trigger-notification', triggerNotification);
 
 export default router;
