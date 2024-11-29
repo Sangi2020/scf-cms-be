38d914ec11cda71f496606cb59f6b224056c30f4
guru-das-pr <gurudaspr.tltechnologies@gmail.com>
2024-11-29 11:53:43 +0530

    updated user routes

--- middlewares/verifyJwtToken.js
+++ middlewares/verifyJwtToken.js
@@ -31,3 +31,17 @@ const verifyJwtToken = (req, res, next) => {
     };
 
 export default verifyJwtToken;
+
+
+export const verifyRole = (requiredRole) => (req, res, next) => {
+  try {
+    if (req.user.role !== requiredRole) {
+      return res
+        .status(403)
+        .json({ message: "Forbidden: You are not authorized to access this route" });
+    }
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: "Internal Server Error", error: err.message });
+  }
+};
--- utils/generateJwtToken.js
+++ utils/generateJwtToken.js
@@ -5,6 +5,7 @@ const generateJwtToken = (user) => {
     const payload = {
         id: user.id,
         email: user.email,
+        role: user.role
     };
 
     return jwt.sign(payload, process.env.JWT_SECRET, {
